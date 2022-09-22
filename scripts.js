402fc29bdc6470d868c066b9fc4470cf9b5a7d0c
fabiandebont <113595400+fabiandebont@users.noreply.github.com>
2022-09-22 15:39:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function apicall() {
 }
 function apicall2() {
     $.ajax({
-        url: "https://cionextgenapi.azure-api.net/test2/specificinfo",
+        url: "https://cionextgenapi.azure-api.net/test2/HttpTrigger2",
         headers: { "Ocp-Apim-Subscription-Key": "3be26df1b0244eed86d86b58bd3b9360"},
         contentType: "application/json",
         type: "GET"
